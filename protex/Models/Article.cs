c5ccd9c2d9bca0d39ea5ac0c31e0bffbd9116535
Tetro7 <67486879+Tetro7@users.noreply.github.com>
2022-05-12 19:53:42 +0300

    add model file Article.cs

---
+++ protex/Models/Article.cs
@@ -0,0 +1,16 @@
+﻿namespace protex.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string AuthorImg { get; set; }
+        public string PublicationDate { get; set; }
+        public string PreviewImg { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+        public string[] Tags { get; set; }
+        public int Watch { get; set; }
+        public int Comments { get; set; }
+    }
+}
