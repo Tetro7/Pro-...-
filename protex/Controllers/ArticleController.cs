00cbafef14742b6d544f202939732f69c9c19619
Tetro7 <67486879+Tetro7@users.noreply.github.com>
2022-05-13 15:38:43 +0300

    finished reading and storing site data in JSON format

---
+++ protex/Controllers/ArticleController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using protex.Models;
+using System.Text.Json;
+
+namespace protex.Controllers
+{
+    public class ArticleController : Controller
+    {
+        public IActionResult Index(int? id)
+        {
+            string pathFolder = "DataJSON/articls";
+            IEnumerable<String> nameArticlFilesJSON = Directory.EnumerateFiles(pathFolder, "*.json", SearchOption.TopDirectoryOnly);
+
+            foreach (string file in nameArticlFilesJSON)
+            {
+                if (file.Contains(id + ".json"))
+                {
+                    using (FileStream fs = new FileStream(file, FileMode.Open))
+                    {
+                        Article? article = JsonSerializer.Deserialize<Article>(fs);
+                        return View(article);
+                    }
+                }
+            }
+
+            return NotFound();
+        }
+    }
+}
